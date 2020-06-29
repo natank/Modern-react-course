db7550affa3014c14e97f880147d84975e00aa93
Nati <natan.kamusher@gmail.com>
2020-06-29 17:32:21 +0300

    saved for remote

--- seasons-app/src/js/components/SeasonDisplay.js
+++ seasons-app/src/js/components/SeasonDisplay.js
@@ -1,8 +1,33 @@
+import '../../styles/SeasonDisplay.css'
 import React from 'react';
 
+const getSeason = (lat, month) => {
+  if(month > 2 && month < 9) {
+    return lat > 0 ? 'summer':'winter';
+  } else {
+    return lat > 0 ? 'winter':'summer';
+  }
+}
+
+const seasonConfig = {
+  summer: {
+    message: 'Let\'s hit the beach!',
+    iconName: 'sun'
+  },
+  winter: {
+    message: 'Burr it is cold!',
+    iconName: 'snowFlake'
+  }
+}
+
 const SeasonDisplay = (props) => {
+  const season = getSeason(props.lat, new Date().getMonth());
+  const {message, iconName} = seasonConfig[season];
   return pug`
-   div Season Display
+   div(className = ${`season-display ${season}`})
+    i(className=${`${iconName} icon`}).massive.icon-left
+    h1 ${message}
+    i(className = ${`${iconName} icon`}).massive.icon-right
   `
 }
 
