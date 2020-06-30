f4e53ab2ec0029cbb4feae850899fd000e670ef1
Nati <natan.kamusher@gmail.com>
2020-06-30 17:30:01 +0300

    saved for remote

--- pics-app/src/js/components/SearchBar.js
+++ pics-app/src/js/components/SearchBar.js
@@ -2,13 +2,17 @@ import React from 'react';
 
 
 class SearchBar extends React.Component {
+    onInputChange() {
+
+    }
+    
     render() {
         return pug`
         .ui.segment
             form.ui.form
                 .field
                 label Image Search
-                input(type="text")
+                input(type="text" style={marginTop: '10px'} onChange=${this.onInputChange})
         `
     }
 }
