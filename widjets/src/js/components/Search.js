b0947de384a380a927d203c8533b6e4724075ed2
Nati <natan.kamusher@gmail.com>
2020-07-05 21:40:27 +0300

    saved for remote

--- widjets/src/js/components/Search.js
+++ widjets/src/js/components/Search.js
@@ -5,7 +5,19 @@ const Search = () => {
   const [results, setResults] = useState([])
   console.log(results);
   useEffect(() => {
-    (async () => {
+    if (term && !results.length) {
+      search()
+    } else {
+      const timeoutId = setTimeout(() => {
+        if (term) search()
+      }, 500)
+      return () => {
+        clearTimeout(timeoutId);
+      }
+    }
+
+
+    async function search() {
       const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
         params: {
           action: 'query',
@@ -16,12 +28,20 @@ const Search = () => {
         }
       })
       setResults(data.query.search);
-    })()
+    }
+
+
   }, [term])
 
   const renderedResults = results.map((result) => {
     return (
       <div className="item" key={result.pageid}>
+        <div className="right floated content">
+          <a
+            className="ui button"
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+          >Go</a>
+        </div>
         <div className="content">
           <div className="header">{result.title}
             <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
