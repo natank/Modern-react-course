c2f0f6abfda614fd9b792bc47f9be38338bee4f7
Nati <natan.kamusher@gmail.com>
2020-08-03 22:29:34 +0300

    saved for remote

--- songs/src/js/components/App.js
+++ songs/src/js/components/App.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { selectSong } from '../actions';
+import SongList from './SongList';
+
 
 const App = () => {
 
   return (
     pug`
       div
-        | App
+        ${SongList}
     `
   )
 
